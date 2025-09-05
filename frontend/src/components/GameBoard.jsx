c5cdbff26fdfa425f77f572204d4b542fefcdacd
Ayushman Kumar <lavi.bhadani@gmail.com>
2025-09-05 18:25:38 +0530

    Trying to deploy

--- frontend/src/components/GameBoard.jsx
+++ frontend/src/components/GameBoard.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-const API_BASE = "http://127.0.0.1:5000";
+const API_BASE = "https://ultimate-ttt-app.onrender.com";
 
 
 const GameBoard = ({ config, onQuit }) => {
